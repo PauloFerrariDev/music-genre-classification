334a57650034424ca7e27fcfce0a6f21de180d4b
Paulo Ferrari <pf.devcon@gmail.com>
2024-05-01 00:57:01 -0300

    first commit

---
+++ audio-handler.py
@@ -0,0 +1,15 @@
+import librosa
+import sounddevice as sd
+import matplotlib.pyplot as plt
+import numpy as np
+
+audio_path = './metal.00034.wav'
+y , sr = librosa.load(audio_path)
+duration = y.size / sr
+time = np.linspace(0, duration, num=y.size)
+print(type(y), type(sr))
+print(y.shape, sr, duration)
+#sd.play(y, sr)
+#sd.wait()
+plt.plot(time, y)
+plt.show()
